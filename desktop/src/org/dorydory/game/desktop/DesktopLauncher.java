9140b1e5c33ea50876fa40e991fff7942e04fe11
ChoiJIN <zuharu@gmail.com>
2015-11-28 22:19:28 +0900

    initial libGDX project

---
+++ desktop/src/org/dorydory/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.dorydory.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.dorydory.game.DoryGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DoryGame(), config);
+	}
+}
