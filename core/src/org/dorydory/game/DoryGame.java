b582fe5794e0de5037ac5eca46fa74441676df8e
ChoiJIN <zuharu@gmail.com>
2015-11-30 16:55:00 +0900

    add freetype for text viewer

--- core/src/org/dorydory/game/DoryGame.java
+++ core/src/org/dorydory/game/DoryGame.java
@@ -1,12 +1,12 @@
 package org.dorydory.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DoryGame extends ApplicationAdapter {
+public class DoryGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
